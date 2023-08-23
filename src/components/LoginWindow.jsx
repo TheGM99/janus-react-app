81818495c12e4a48839f4859e4f8873e74f84cdd
Gregory Matczak <grzesmatczak@gmail.com>
2023-08-23 07:43:27 +0200

    add IDs

--- src/components/LoginWindow.jsx
+++ src/components/LoginWindow.jsx
@@ -5,12 +5,14 @@ function LoginWindow({ handleClick, setRoomId, setUsername }) {
     <div className="bg-primary text-white text-poppins mx-2 w-[90%]  sm:w-4/6 md:w-1/2 lg:w-2/5 flex flex-col px-6 ss:px-[5%] rounded-xl">
       <label className="text-2xl mt-6 ml-2">Username</label>
       <input
+        id="usernameInput"
         type="text"
         className="bg-secondary border-highlight border-2 p-2 rounded-xl text-lg"
         onChange={(e) => setUsername(e.target.value)}
       ></input>
       <label className="text-2xl mt-6 ml-2">Room ID</label>
       <input
+        id="roomInput"
         type="text"
         className="bg-secondary border-highlight border-2 p-2 rounded-xl text-lg"
         placeholder="(Join Only)"
@@ -18,6 +20,7 @@ function LoginWindow({ handleClick, setRoomId, setUsername }) {
       ></input>
       <div className="my-6 flex flex-col space-y-3 ss:flex-row ss:space-y-0 ss:justify-between">
         <button
+          id="createButton"
           className="bg-green px-5 py-2 rounded-xl font-bold hover:bg-hover active:translate-y-1 ss:text-xl"
           onClick={() => handleClick(true)}
         >
@@ -25,6 +28,7 @@ function LoginWindow({ handleClick, setRoomId, setUsername }) {
           CREATE
         </button>
         <button
+          id="joinButton"
           className="bg-green px-8 py-2 rounded-xl font-bold hover:bg-hover active:translate-y-1 ss:text-xl"
           onClick={() => handleClick(false)}
         >
